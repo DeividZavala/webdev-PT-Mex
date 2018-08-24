83d6a43db2a786eda9afe56ce9731578c3371c51
DeividZavala <crusedmoss@gmail.com>
2018-08-23 20:19:07 -0500

    animacion papud

--- semana-cinco/jueves/main.js
+++ semana-cinco/jueves/main.js
@@ -15,11 +15,15 @@ class Mario{
         this.y = 20;
         this.width = 30;
         this.height = 40;
-        this.image = new Image();
-        this.image.src = "https://bit.ly/2L7yH3f"
+        this.image1 = new Image();
+        this.image1.src = "https://bit.ly/2L7yH3f";
+        this.image2 = new Image();
+        this.image2.src = "https://bit.ly/2L3ikoe";
+        this.image = this.image1;
     }
 
     draw(){
+        if(frames % 10 === 0 ) this.image = this.image === this.image1 ? this.image2 : this.image1;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
 
@@ -27,6 +31,13 @@ class Mario{
 
 var mario = new Mario();
 
-mario.image.onload = function(){
+/*mario.image.onload = function(){
     mario.draw();
-}
+}*/
+
+var frames = 0;
+setInterval(function(){
+    frames++;
+    ctx.clearRect(0,0, 400, 200);
+    mario.draw();
+}, 1000/60);
