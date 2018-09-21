6ecc89bc8fbc89f814811bc44c6643c9233820f5
DeividZavala <crusedmoss@gmail.com>
2018-09-20 19:22:11 -0500

    npm and nodejs

---
+++ novena-semana/index.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+
+console.log(chalk.red("que onda perro desde la terminal") + chalk.green("que onda papud"));
+
+
+for(var i = 0; i<11; i++){
+
+	console.log(i)
+
+}
