8f6f1c31ad63ef0b62a2a860da5739a8f724413e
DeividZavala <crusedmoss@gmail.com>
2018-09-29 16:56:55 -0500

    showing movies

--- decima-semana/sabado/app.js
+++ decima-semana/sabado/app.js
@@ -13,11 +13,32 @@ mongoose.connect('mongodb://localhost:27017/video', {useNewUrlParser: true})
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
+app.get('/', (req, res) => {
+        Movie.find().limit(20)
+            .then(movies => {
+                res.render('home', {movies});
+            });
+});
+
+
+// Route params
 app.get('/products/:prod', (req, res) => {
         Movie.find({_id: req.params.prod})
             .then(movies => {
                 res.json({movies, params: req.params});
             });
+});
+
+
+// Query params
+app.get('/products', (req, res) => {
+        //  /producs?perro=panzer&age=20
+
+        Movie.find({_id:req.query.id})
+            .then(movies => {
+                    res.json({movies})
+            });
+
 });
 
 app.listen("3000", () => {
