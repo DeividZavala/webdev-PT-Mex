411954dba15b547ea8ea7ff772c1d8efaf26774b
DeividZavala <crusedmoss@gmail.com>
2018-10-09 21:33:37 -0500

    roles

--- semana-12/martes/roles/routes/index.js
+++ semana-12/martes/roles/routes/index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router  = express.Router();
 
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()) return next();
+    res.redirect("/auth/login");
+}
+
+function checkRole(role){
+  return function (req, res, next){
+    if(req.isAuthenticated() && req.user.role === role) return next();
+    res.redirect("/private");
+  }
+}
+
+const checkAdmin = checkRole("ADMIN");
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get("/private", (req, res) => {
+router.get("/private", isLoggedIn, (req, res) => {
   res.render("private", {user: req.user});
+});
+
+router.get("/admin", checkAdmin, (req, res) => {
+    res.render("private", {user: req.user});
 });
 
 module.exports = router;
