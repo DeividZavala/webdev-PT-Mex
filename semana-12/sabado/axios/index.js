5ab6f308c18afa3efa815e038e3fb6eec3876c65
DeividZavala <crusedmoss@gmail.com>
2018-10-13 12:24:47 -0500

    request dinamica

--- semana-12/sabado/axios/index.js
+++ semana-12/sabado/axios/index.js
@@ -1,7 +1,23 @@
 
-axios.get("https://pokeapi.co/api/v2/pokemon/pikachu/")
+function getPokeData(e){
+    axios.get(e.target.dataset.pokeurl)
+        .then(res => {
+            document.getElementById("name").innerHTML = res.data.name;
+            document.getElementsByTagName("img")[0].src = res.data.sprites.front_default;
+        })
+}
+
+
+
+axios.get("https://pokeapi.co/api/v2/pokemon/")
 .then(res => {
     console.log(res.data);
-    document.getElementById("name").innerHTML = res.data.name;
-    document.getElementsByTagName("img")[0].src = res.data.sprites.front_default;
+    let list = document.getElementById("list")
+    res.data.results.forEach(pokemon => {
+        let li = document.createElement("li");
+        li.dataset.pokeurl = pokemon.url;
+        li.innerHTML = pokemon.name;
+        li.onclick = getPokeData;
+        list.appendChild(li);
+    })
 })
