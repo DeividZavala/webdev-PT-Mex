4eeb2f817fe6ebc840383d3ec458b4d836d9cbd1
DeividZavala <crusedmoss@gmail.com>
2018-10-20 15:35:58 -0500

    git ad

--- subastas/routes/auth.js
+++ subastas/routes/auth.js
@@ -8,7 +8,7 @@ router.get("/login", (req, res) => {
 });
 
 router.post("/login",  passport.authenticate("local", {
-    successRedirect: "/",
+    successRedirect: "/bid",
     failureRedirect: "/auth/login"
 }));
 
--- subastas/routes/bid.js
+++ subastas/routes/bid.js
@@ -7,6 +7,19 @@ function isLoggedIn(req, res, next){
     res.redirect("/auth/login");
 }
 
+function checkIfOwner(req, res, next){
+    Bid.findById(req.params.id)
+        .then(bid => {
+            if(bid.owner.toString() === req.user._id.toString()){
+                req.bid = bid;
+                next();
+            }
+        })
+        .catch(() => {
+            res.redirect("/bid");
+        });
+}
+
 router.get("/", isLoggedIn, (req, res) => {
     Bid.find()
         .then(bids => {
@@ -18,6 +31,20 @@ router.get("/new", isLoggedIn,(req, res) => {
     res.render("bid-form");
 });
 
+router.get("/:id/edit",isLoggedIn, checkIfOwner, (req, res) => {
+    const {bid} = req;
+    res.render("bid-form",{bid});
+});
+
+// editando el post
+router.post("/:id/edit", isLoggedIn, checkIfOwner,(req, res) => {
+
+    Bid.findByIdAndUpdate(req.params.id, {$set: req.body})
+        .then(()=>{
+            res.redirect("/bid")
+        })
+});
+
 router.post("/new", isLoggedIn, (req, res) => {
     req.body.owner = req.user._id;
     Bid.create(req.body)
@@ -25,7 +52,6 @@ router.post("/new", isLoggedIn, (req, res) => {
             res.redirect("/bid")
         })
         .catch(err => {
-            console.log(err);
             res.render("bid-form", {err, msg:"No se pudo crear tu subasta"})
         })
 });
