2172db9f55745360338fe49974a17f50e32c262b
DeividZavala <crusedmoss@gmail.com>
2018-11-24 17:11:25 -0600

    login

--- semana-18/back-front/back-medium/routes/auth.js
+++ semana-18/back-front/back-medium/routes/auth.js
@@ -1,7 +1,47 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-router.post("/");
+router.post("/login", async (req, res) => {
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(404).json({msg: "Email no es valido"});
+
+    let validPassword = bcrypt.compareSync(req.body.password, user.password);
+
+    if(!validPassword) return res.status(500).json({msg:"La contra es incorrecta"});
+
+    const token = jwt.sign(
+        {id: user._id},
+        process.env.SECRET,
+        {
+        expiresIn: 8600
+    });
+
+    res.status(200).json({user, token});
+
+});
+
+router.post("/register", (req, res) => {
+
+    if(req.body.password !== req.body.confirmPassword) return res.status(500).json({msg: "Las cotraseñas no coinciden"});
+
+    const salt = bcrypt.genSaltSync(256);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword
+    })
+        .then(() => {
+            res.status(201).json({msg: "Usuario creado con éxito"})
+        })
+        .catch(err => {
+            res.status(500).json({err, msg: "No se pudo crear"})
+        })
+
+});
 
 module.exports = router;
